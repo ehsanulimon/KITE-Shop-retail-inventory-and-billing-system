699e22cebd6818583ab967e5cb5710d2c0a735ee
Ehsanul Mostafa Imon <mostafaimon012020@gmail.com>
2022-08-01 00:19:48 +0600

    loginpage

---
+++ kite/Login.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kite
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void exitbtnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RegistrationbtnClick(object sender, RoutedEventArgs e)
+        {
+              login_gridX.Visibility = Visibility.Collapsed;
+            Registration_gridX.Visibility = Visibility.Visible;
+        }
+        private void LoginbtnClick(object sender, RoutedEventArgs e)
+        {
+           
+            Registration_gridX.Visibility = Visibility.Collapsed;
+            login_gridX.Visibility = Visibility.Visible;
+        }
+
+        private void Submitbtn_regClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clearbtn_regClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ kite/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+
+namespace kite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        
+        }
+
+        /// <temp>
+        /// //// 
+       
+        private void nextbtnClick(object sender, RoutedEventArgs e)
+        {
+            Login op = new Login();
+            op.Show();
+            Close();
+        }
+        /// <end temp>
+        /// //// 
+
+        // Start / Stop Timer
+
+        DispatcherTimer _timer = new DispatcherTimer();
+        int counter = 0;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            TimerLabel.Text = counter.ToString();
+
+            if (counter == 100)
+            {
+               
+                _timer.Stop();
+                TimerLabel.Text = "0".ToString();
+            
+                Login op = new Login();
+                op.Show();
+                Close();
+            }
+          
+        }
+
+        private void StartTimer()
+        {
+            cpb_uc.Visibility = Visibility.Visible;
+
+            if (counter > 0)
+            {
+                _timer.Tick -= timer_Tick;
+                counter = 0;
+            }
+
+            _timer.Interval = TimeSpan.FromMilliseconds(188);
+            _timer.Tick += timer_Tick;
+            _timer.Start();
+           
+        }
+
+        private void StopTimer()
+        {
+            if (counter > 0)
+            {
+                _timer.Tick -= timer_Tick;
+                counter = 0;
+            }
+
+            _timer.Stop();
+            cpb_uc.Visibility = Visibility.Collapsed;
+            TimerLabel.Text = "0".ToString();
+         
+        }
+
+     
+          private void StartAnimation()
+            {
+                ((Storyboard)cpb_uc.Resources["ProgressBarAnimation"]).Begin();
+            }
+
+            private void StopAnimation()
+            {
+                ((Storyboard)cpb_uc.Resources["ProgressBarAnimation"]).Stop();
+            }
+
+        private void loaded(object sender, RoutedEventArgs e)
+
+        {
+        
+            StartTimer();
+            StartAnimation();
+        }
+
+
+    }
+}
